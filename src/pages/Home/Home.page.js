f3bb899f1e5c4b1ff7c79696d1f12d487ca9a389
jacobo4 <chandas.2001@gmail.com>
2021-07-09 15:42:49 -0500

    asd

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -14,7 +14,7 @@ class HomePage extends Component {
                     <div className="hero-title-wrapper">
                         <h1 className="hero__title">Un buen título</h1>
 
-                        <p className="hero__description">Lorem Ipsum is simply dummy text of the printing and
+                        <p className="hero__description">Lsorem Ipsum is simply dummy text of the printing and
                             typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
                             1500s, when an unknown printer took a galley of type and scrambled it to make a type
                             specimen book.</p>
